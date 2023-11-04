1423722aac44578b2b3e627b854c19b0487a9ecb
angiechiessa2022 <114378783+angiechiessa2022@users.noreply.github.com>
2023-11-04 17:11:44 -0600

    Delete Portafolio directory

--- Portafolio/purgar.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-      require('postcss-purgecss')({
-        content: ['./index.html', './src/**/*.vue', './src/**/*.js'],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      }),
-    ],
-  };
-  
